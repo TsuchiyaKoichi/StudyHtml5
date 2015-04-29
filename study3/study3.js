9702e521db3b5585089092ce05eccfe6050a54ca
lival1116 <lival1116@gmail.com>
2015-04-29 21:11:37 +0000

    Added section 3

--- study3/study3.js
+++ study3/study3.js
@@ -1,1 +1,36 @@
-angular.module('app', []);
+angular.module('app', ['ngRoute'])
+    .config(['$routeProvider', function(rootProvider) {
+        
+        rootProvider
+            .when('/', {
+                templateUrl: 'main.html',
+                controller: 'MainController'
+            })
+            .when('/page1/:name', {
+                templateUrl: 'page1.html',
+                controller: 'Page1Controller'
+            })
+            .when('/page2', {
+                templateUrl: 'page2.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }])
+    // filter定義は自分で作成することもできる
+    .filter('reverse', function() {
+       return function(values) { return values.concat().reverse(); }; 
+    })
+    .controller('MainController', ['$scope', function(scope) {
+        
+        scope.msg = 'This is main template.';
+        scope.pages = [
+            { url: '#/page1/tsuchiya', title: 'Page1' },
+            { url: '#/page2', title: 'Page2' },
+            { url: '#/page3', title: 'Page3' }
+        ];
+    }])
+    .controller('Page1Controller', ['$scope', '$routeParams', 'reverse', function(scope, routeParams, reverse) {
+        scope.msg = 'This is page1 template.';
+        scope.name = routeParams.name;
+    }]);
